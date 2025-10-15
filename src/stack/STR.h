3b513f43adeaa5202dd92adcec8f327064845126
RomanSemenskiy <st142067@student.spbu.ru>
2025-10-15 12:03:40 +0300

    Add comments

--- src/stack/STR.h
+++ src/stack/STR.h
@@ -6,10 +6,10 @@ typedef struct obj{
     struct obj* next;
 }Elem;
 
-Elem* new(void);
-Elem* push(Elem* top , char data);
-char pop(Elem** top);
-void peek(Elem* top);
-void delete(Elem** top);
+Elem* new(void); //создаю голову, указатель указывает на NULL
+Elem* push(Elem* top , char data); //добавляю на вершину стэка новый элемент, при этом функция возвращает указатель на самый верхний элемент
+char pop(Elem** top); //удаляется верхний элемент, при этом возвращается его значение
+void peek(Elem* top); //просто показывает самый верхний элемент
+void delete(Elem** top);//очищает стэк
 
 #endif
