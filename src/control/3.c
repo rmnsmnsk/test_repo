16f7436214b0fb7aeeeda7f9e2f8853a06b335d9
RomanSemenskiy <st142067@student.spbu.ru>
2025-10-17 17:29:18 +0300

    Add the things that i forgotten

--- src/control/3.c
+++ src/control/3.c
@@ -5,46 +5,28 @@ int main(void) {
 
     printf("Введите число: ");
     scanf("%d", &n);
-    int flag = 1;
 
     for (int j = 1; j <= n; ++j) {
-        for (int b = j + 1; b <= n; ++b) {
-            flag = 1;
-            int char1[100];
-            int char2[100];
-            int l = 0;
-            int h = 0;
-
-            int tempj = j;
-            int tempb = b;
-
-            while (tempj > 0) {
-                char1[l] = (tempj % 2);
-                ++l;
-                tempj /= 2;
-            }
-
-            while (tempb > 0) {
-                char2[h] = (tempb % 2);
-                ++h;
-                tempb /= 2;
-            }
-
-            if (l == h) {
-                for (int d = 0; d < l; ++d) {
-                    if (char1[d] != char2[l - 1 - d]) { 
-                        flag = 0;
-                        break;
-                    }
-                }
-            } 
-            else {
+        int digits[100];
+        int length = 0;
+        
+        int temp = j;
+        while (temp > 0) {
+            digits[length] = (temp % 2);
+            ++length;
+            temp /= 2;
+        }
+        
+        int flag = 1;
+        for (int i = 0; i < length / 2; ++i) {
+            if (digits[i] != digits[length - 1 - i]) {
                 flag = 0;
+                break;
             }
-            
-            if (flag == 1) {
-                printf("%d %d\n", j, b); 
-            }
+        }
+
+        if (flag == 1) {
+            printf("%d\n", j);
         }
     }
     
