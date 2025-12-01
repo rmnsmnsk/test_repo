fbc0e0e93f6ad56e0c004c50fcdb9ca384852eb0
RomanSemenskiy <st142067@student.spbu.ru>
2025-12-01 20:19:57 +0300

    Formatation

--- src/12.2_c/12.c
+++ src/12.2_c/12.c
@@ -1,30 +1,31 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 typedef union Number {
     double value;
-    uint64_t bits; //для 64 бит, в памяти double - (1 бит - знак, след 11 - экспонента, оставшиеся 52 - мантисса)
+    uint64_t bits; // для 64 бит, в памяти double - (1 бит - знак, след 11 - экспонента, оставшиеся 52 - мантисса)
 } Number;
 
-int main() {
+int main()
+{
     Number num;
 
     printf("Введите число double: ");
     scanf("%lf", &num.value);
 
     uint64_t b = num.bits;
-    int s = (b >> 63) & 1; //знак
-    int exp = (b >> 52) & 2047; //экспонента, 2047 - 11 единиц
-    uint64_t mbits = b & 4503599627370495; //4503599627370495 - 52 единицы
+    int s = (b >> 63) & 1; // знак
+    int exp = (b >> 52) & 2047; // экспонента, 2047 - 11 единиц
+    uint64_t mbits = b & 4503599627370495; // 4503599627370495 - 52 единицы
 
     if (exp == 0 && mbits == 0) {
         printf("Результат: +0.0*2^0\n");
         return 0;
     }
 
-    int rexp = exp - 1023; //в памти хранятся со сдвигом
+    int rexp = exp - 1023; // в памти хранятся со сдвигом
 
-    mbits |= (1ULL << 52); //добавляю скрытую единицу, а 1ULL - просто 1 в 64 битах
+    mbits |= (1ULL << 52); // добавляю скрытую единицу, а 1ULL - просто 1 в 64 битах
 
     double m = 0.0;
     double w = 1.0;
