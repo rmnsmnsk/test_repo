424f382fad64c57c8df5e9fab5be0ef7c8abd2d8
Roman_Semensky <136500209+rmnsmnsk@users.noreply.github.com>
2025-12-20 04:27:51 +0300

    Update curry.py

--- src/curry.py
+++ src/curry.py
@@ -1,7 +1,12 @@
-def curry(f, arity):
+def curry(func, n):
+    if not isinstance(n, int):
+        raise TypeError("n должно быть целым числом")
+    if n < 0:
+        raise ValueError("n должно быть неотрицательным числом")
+    
     def curried(*args):
-        if len(args) >= arity:
-            return f(*args)
+        if len(args) >= n:
+            return func(*args)
         else:
             def next_func(next_arg):
                 return curried(*args + (next_arg,))
@@ -13,16 +18,16 @@ def uncurry(curried_func, arity):
     def uncurried_func(*args):
         if len(args) != arity:
             raise ValueError(f"Ожидается {arity} аргументов, получено {len(args)}")
-        
+
         result = curried_func
         for arg in args:
             result = result(arg)
         return result
-    
+
     return uncurried_func
 
 
-def sum(x, y, z):
+def add_three_numbers(x, y, z):
     return x + y + z
 
 
@@ -31,11 +36,11 @@ def sub(x, y):
 
 
 if __name__ == "__main__":
-    sum3_curry = curry(sum, 3)
+    sum3_curry = curry(add_three_numbers, 3)
     sum3_uncurry = uncurry(sum3_curry, 3)
 
     print(sum3_curry(1)(2)(3))
     print(sum3_uncurry(1, 2, 3))
-
+ 
     sub_curry = curry(sub, 2)
-    print(sub_curry(10)(2))
+    print(sub_curry(10)(2)) 
